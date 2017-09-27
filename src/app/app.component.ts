4f8d9d5f4e2a31fabf15ddeab2e715a8565675ff
kkorezelidis <kostaskorezelidis@Kostass-MacBook-Pro.local>
2017-09-28 00:42:22 +0300

    [master] add sidenav menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { Router, NavigationStart } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  @ViewChild('sidenav') sidenav;
+  constructor(private router: Router) {
+    console.log('constructor called');
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationStart) {
+        this.sidenav.close();
+      }
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,46 @@
+import { RouterModule, Routes } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MdButtonModule, MdCheckboxModule, MatDatepickerModule, MdNativeDateModule} from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  MdButtonModule,
+  MdCheckboxModule,
+  MatDatepickerModule,
+  MdNativeDateModule,
+  MdIconModule,
+  MdIconRegistry,
+  MdSidenavModule,
+  MdListModule
+} from '@angular/material';
+import { AppComponent } from './app.component';
+import { HomePageComponent } from './homepage/homepage';
+import { LeaderBoardComponent } from './leaderboard/leaderboard';
 import 'hammerjs';
 
-
-import { AppComponent } from './app.component';
+const appRoutes: Routes = [
+  { path: '', redirectTo: 'homepage', pathMatch: 'full' },
+  { path: 'homepage', component: HomePageComponent },
+  { path: 'leaderboard', component: LeaderBoardComponent }
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, LeaderBoardComponent, HomePageComponent],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule,
     BrowserAnimationsModule,
     MdButtonModule,
     MdCheckboxModule,
     MatDatepickerModule,
-    MdNativeDateModule
+    MdNativeDateModule,
+    MdIconModule,
+    MdSidenavModule,
+    MdListModule
   ],
-  providers: [],
+  providers: [MdIconRegistry],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
