09741371b60ba3903dcc46ebba0da96ef0b86f1a
kkorezelidis <kostaskorezelidis@Kostass-MacBook-Pro.local>
2017-09-27 21:53:05 +0300

    [master] Add material design.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MdButtonModule, MdCheckboxModule, MatDatepickerModule, MdNativeDateModule} from '@angular/material';
+import 'hammerjs';
+
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +12,12 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MdButtonModule,
+    MdCheckboxModule,
+    MatDatepickerModule,
+    MdNativeDateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
