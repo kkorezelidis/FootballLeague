b34b4ad9fcc1deb2b79c846db6fc320ad0769086
kkorezelidis <kostaskorezelidis@Kostass-MacBook-Pro.local>
2017-09-28 00:41:14 +0300

    [master] add leaderboard component

---
+++ src/app/leaderboard/leaderboard.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-leaderboard',
+  templateUrl: './leaderboard.html',
+  styleUrls: ['./leaderboard.scss']
+})
+export class LeaderBoardComponent {}
